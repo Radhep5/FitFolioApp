c8879f79e633f845d14c7f7a22ecdd7d1a73eead
Radhep5 <147786241+Radhep5@users.noreply.github.com>
2024-02-22 14:13:13 -0600

    box

--- fitfoliofinal/Screens/TrackerScreen.js
+++ fitfoliofinal/Screens/TrackerScreen.js
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 import styles from "./style";
+import {
+  Alert,
+  Keyboard,
+  KeyboardAvoidingView,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+  Image,
+} from "react-native";
 
-const TrackerScreen = () => {};
+const TrackerScreen = () => {
+  return <View style={styles.box}></View>;
+};
 
 export default TrackerScreen;
--- fitfoliofinal/Screens/style.js
+++ fitfoliofinal/Screens/style.js
@@ -8,5 +8,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: "#000000",
+    border: 1,
+    margin: 20,
+  },
 });
 export default styles;
