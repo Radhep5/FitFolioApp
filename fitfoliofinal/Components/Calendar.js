ed396ea9b274e8740c0d5ba7e508ab20c5e31e24
Achyut Patel <137658082+TRshaqOat@users.noreply.github.com>
2024-02-22 23:17:26 -0600

    calendar done

--- fitfoliofinal/Components/Calendar.js
+++ fitfoliofinal/Components/Calendar.js
@@ -1,15 +1,51 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Text, Button, Platform } from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 const Calendar = () => {
-  return <View style={styles.container}>{/* Your content goes here */}</View>;
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState("date");
+  const [text, setText] = useState("Empty");
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.os === "ios");
+    setDate(currentDate);
+
+    let tempDate = new Date(currentDate);
+    let fDate =
+      tempDate.getMonth() +
+      1 +
+      "/" +
+      tempDate.getDate() +
+      "/" +
+      tempDate.getFullYear();
+    setText(fDate);
+
+    console.log(fDate);
+  };
+
+  return (
+    <View style={styles.container}>
+      <DateTimePicker
+        testID="dateTimePicker"
+        alignSelf="center"
+        value={date}
+        mode={mode}
+        is24Hour={true}
+        display="default"
+        onChange={onChange}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#3F9DF3",
-    borderRadius: 11, // Adjust the value to change the curvature
-    padding: 20,
+    borderRadius: 11,
+    padding: 7,
     margin: 10,
     width: 300,
     alignSelf: "center",
