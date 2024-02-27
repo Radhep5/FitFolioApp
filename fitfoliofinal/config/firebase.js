438317d3421a0ed0700d1a9e6d0c849398a94eed
Radhep5 <147786241+Radhep5@users.noreply.github.com>
2024-02-26 22:46:43 -0600

    firebaseConfig

---
+++ fitfoliofinal/config/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9pUvcLebr3puCEasDG5ybqTtYPVgfCOM",
+  authDomain: "fitfoliodb.firebaseapp.com",
+  projectId: "fitfoliodb",
+  storageBucket: "fitfoliodb.appspot.com",
+  messagingSenderId: "532282277003",
+  appId: "1:532282277003:web:c716398275df5480baa88e",
+  measurementId: "G-2J885T04R8",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const trackerDB = getFirestore(app);
+export { trackerDB };
