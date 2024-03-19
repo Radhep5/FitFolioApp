e7a2dc4abf00c2d3f56c896ca02b75ea6af68f4f
Achyut Patel <137658082+TRshaqOat@users.noreply.github.com>
2024-03-19 14:37:24 -0500

    looks

--- fitfoliofinal/Screens/AccountScreen.js
+++ fitfoliofinal/Screens/AccountScreen.js
@@ -1,12 +1,43 @@
 import React, { useState } from "react";
-//import styles from "./style";
-
-import { View, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  Modal,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ScrollView,
+} from "react-native";
 
 const AccountScreen = ({ navigation }) => {
   return (
     <View style={[styles.container, { backgroundColor: "#1E1E1E" }]}>
-      <View style={styles.testBox}></View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.name}>Name</Text>
+        <TouchableOpacity style={styles.accountButtons}>
+          <Text style={styles.accountButtonsText}>
+            Edit Info. {"                                ＞"}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.accountButtons}>
+          <Text style={styles.accountButtonsText}>
+            Notifications {"                         ＞"}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.accountButtons}>
+          <Text style={styles.accountButtonsText}>
+            Help / Contact {"                      ＞"}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.accountButtons}>
+          <Text style={styles.accountButtonsText}>
+            Log Out {"                                 ＞"}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -14,8 +45,39 @@ const AccountScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    paddingTop: 60,
     alignItems: "center",
+  },
+  infoContainer: {
+    width: 306,
+    height: 315,
+    backgroundColor: "#2D2D2D",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#7A7A7A",
+    alignItems: "center",
+  },
+  name: {
+    position: "absolute",
+    top: 25,
+    left: 20,
+    fontSize: 32,
+    color: "white",
+  },
+  accountButtons: {
+    top: 100,
+    borderColor: "#7A7A7A",
+    borderTopWidth: 1,
+    width: 306,
+    height: 53,
+    borderColor: "#7A7A7A",
+    backgroundColor: "transparent",
+    justifyContent: "center",
+  },
+  accountButtonsText: {
+    left: 30,
+    fontSize: 20,
+    color: "white",
   },
 });
 
